91a013d9be2aba4d5c61322da467e5860fae24f8
ErShubham4u <connect2ershubham@gmail.com>
2023-12-16 08:56:22 +0530

    triangular pattern of zeros

---
+++ P02.java
@@ -0,0 +1,10 @@
+public class P02 {
+    public static void main(String args[]) {
+        for(int i = 0 ; i <= 4 ; i++){
+            for(int j = 0 ; j < i + 1 ; j++){
+                System.out.print(0);
+            }
+            System.out.println();
+        }
+      }
+}

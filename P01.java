3b5f2bf9e062adc843d42201e00bc2fc26b6a5d3
ErShubham4u <connect2ershubham@gmail.com>
2023-12-16 08:55:03 +0530

    rectangular pattern

---
+++ P01.java
@@ -0,0 +1,11 @@
+public class P01 {
+
+    public static void main(String args[]) {
+        for(int i = 0 ; i <= 4 ; i++){
+            for(int j = 0 ; j <= 5 ; j++){
+                System.out.print(0);
+            }
+            System.out.println();
+        }
+      }
+}
